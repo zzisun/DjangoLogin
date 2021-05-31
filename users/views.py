bff1b0c2c32008aa9d774411d2466c56079d8e05
Ji-Sun Kim <jisunkim98@gmail.com>
2021-05-31 10:54:33 +0900

    Feat: UserCreateForm

--- users/views.py
+++ users/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.hashers import make_password
+from users.models import User
 from users.forms import RegisterForm
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
@@ -11,34 +13,20 @@ def mypage(request):
     return render(request, "users/status.html")
 
 def registerPage(request):
-	if request.user.is_authenticated:
-		return redirect('status')
-	else:
-		form = RegisterForm()
-		if request.method == 'POST':
+	form = RegisterForm()
+	if request.method == 'POST':
 			form = RegisterForm(request.POST)
-			if form.is_valid():
-				form.save()
-				user = form.cleaned_data.get('username')
-				messages.success(request, 'Account was created for ' + user)
+			user = User(
+           		email=form.data.get('email'),
+            	name=form.data.get('name'),
+            	password=make_password(form.data.get('password')),
+            	level='user'
+        	)
+			user.save()
+			username = form.cleaned_data.get('first_name')
+			messages.success(request, 'Account was created for ' + user)
 
-		context = {'form':form}
-		return render(request, 'users/sign_up1.html', context)
+			return redirect('verify')
+	context = {'form':form}
+	return render(request, 'users/sign_up1.html', context)
 
-# def registerPage(request):
-#     """
-#     계정생성
-#     """
-#     if request.method == "POST":
-#         form = RegisterForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             username = form.cleaned_data.get('username')
-
-#             raw_password = form.cleaned_data.get('password1')
-#             # user = authenticate(username=username, password=raw_password)
-#             # login(request, user)
-#             return redirect('index')
-#     else:
-#         form = RegisterForm()
-#     return render(request, 'users/sign_up1.html', {'form': form})
